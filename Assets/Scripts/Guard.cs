1ebeaf117dbb5d069c4da35d9e08df1d546179eb
pmm4654 <pmm4654@gmail.com>
2019-12-18 14:40:10 -0700

    Initial commit

---
+++ Assets/Scripts/Guard.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    public static event System.Action OnGuardHasSpottedPlayer;
+
+    public float speed = 5;
+    public float waitTime = .3f;
+    public float turnSpeed = 90; // 90 deg / second
+
+    public float timeToSpotPlayer = .5f;
+    float playerVisibleTimer;
+    public Transform pathHolder;
+    
+    public Light spotlight;
+    public float viewDistance;
+    float viewAngle;
+
+    public LayerMask viewMask;
+    Transform player;
+    Color originalSpotlightColor;
+
+    private void Start()
+    {
+        originalSpotlightColor = spotlight.color;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        viewAngle = spotlight.spotAngle;
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+        for(int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+        }
+        StartCoroutine(FollowPath(waypoints));
+    }
+
+    private void Update()
+    {
+        if(CanSeePlayer())
+        {
+            playerVisibleTimer += Time.deltaTime;
+            spotlight.color = Color.red;
+        } else
+        {
+            playerVisibleTimer -= Time.deltaTime;
+            spotlight.color = originalSpotlightColor;
+        }
+        playerVisibleTimer = Mathf.Clamp(playerVisibleTimer, 0, timeToSpotPlayer);
+        spotlight.color = Color.Lerp(originalSpotlightColor, Color.red, playerVisibleTimer / timeToSpotPlayer);
+
+        if(playerVisibleTimer >= timeToSpotPlayer)
+        {
+            if(OnGuardHasSpottedPlayer != null)
+            {
+                OnGuardHasSpottedPlayer();
+            }
+        }
+    }
+
+    bool CanSeePlayer()
+    {
+        if (Vector3.Distance(transform.position, player.position) < viewDistance)
+        {
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            float angleBetweenGuardAndPlayer = Vector3.Angle(transform.forward, dirToPlayer);
+            //(angle returns smallest angle (so instead of 210, it would return 30 so you can just check is less than 2f)
+            if (angleBetweenGuardAndPlayer < viewAngle / 2f) // are you inside of the flashlight range 
+            {
+                if (!Physics.Linecast(transform.position, player.position, viewMask)) // if you haven't hit any other obstacles, then you can see the player
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        foreach(Transform waypoint in pathHolder)
+        {
+            float gizmoRadius = .3f;
+            Gizmos.DrawSphere(waypoint.position, gizmoRadius);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
+    }
+
+    IEnumerator FollowPath(Vector3[] waypoints) 
+    {
+        transform.position = waypoints[0];
+
+        int targetWaypointIndex = 1;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint); // make so that the target initially faces the first waypoint
+
+        while(true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+            if(transform.position == targetWaypoint) // you have reached your target waypoint
+            {
+                // when your current index is equal to the number of waypoints, it goes back to 0 and loops through the path again
+                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+
+                yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint)); // wait for this coroutine to stop before continuing
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = ToUnityCircleAngles(Mathf.Atan2(dirToLookTarget.z, dirToLookTarget.x) * Mathf.Rad2Deg);
+        while(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle)) > 0.05f) { // don't use 0 because it might no actually get all the way to 0
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+            yield return null;
+        }
+    }
+    
+
+    float ToUnityCircleAngles(float angle)
+    {
+        return 90 - angle;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public event System.Action OnReachedEndOfLevel;
+
+    public float speed = 7;
+    public float smoothMoveTime = .1f;
+    public float turnSpeed = 8f;
+
+    float currentAngle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    Vector3 velocity;
+
+    Rigidbody rigidbody;
+    bool disabled;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Guard.OnGuardHasSpottedPlayer += Disable;
+    }
+
+    void Update()
+    {
+        Vector3 inputDirection = Vector3.zero;
+        if(!disabled)
+        {
+            inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float inputAngle = ToUnityCircleAngles(Mathf.Atan2(inputDirection.z, inputDirection.x) * Mathf.Rad2Deg);
+
+        // stop interpolation by multiplying by inputMagnitude so that the angle doesn't change when you stop pressing a direction
+        currentAngle = Mathf.LerpAngle(currentAngle, inputAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+        velocity = transform.forward * speed * smoothInputMagnitude;
+        // handle rotation with rigidbody
+        //transform.eulerAngles = Vector3.up * currentAngle;
+        //transform.Translate(inputDirection * Time.deltaTime * speed * smoothInputMagnitude, Space.World);
+    }
+    
+    // RigidBodyies need to be updated in the fixed update method
+    private void FixedUpdate()
+    {
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * currentAngle));
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime );
+    }
+
+    private void Disable()
+    {
+        disabled = true;
+    }
+
+    float ToUnityCircleAngles (float angle)
+    {
+        return 90 - angle;
+    }
+
+    private void OnTriggerEnter(Collider hitCollider)
+    {
+        if(hitCollider.tag == "Finish")
+        {
+            Disable();
+            if(OnReachedEndOfLevel != null)
+            {
+                OnReachedEndOfLevel();
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Guard.OnGuardHasSpottedPlayer -= Disable;
+    }
+}
+
